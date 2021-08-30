0d3776d8d569841ebbdcaf15fecfccabc558c68e
trungnguyen <77141985+andromeda9096@users.noreply.github.com>
2021-08-30 10:40:10 +0700

    Add files via upload

---
+++ dockeronubuntu.sh
@@ -0,0 +1,19 @@
+sudo apt-get update -yy
+
+sudo apt-get install -yy apt-transport-https ca-certificates curl gnupg lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg --yes
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update -yy
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io -yy
+
+sudo groupadd docker
+
+sudo usermod -aG docker $USER
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
